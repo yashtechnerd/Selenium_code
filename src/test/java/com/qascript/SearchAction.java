3a6822f0e932ac38c359c352ff7a7580077891e4
yashtechnerd <153451122+yashtechnerd@users.noreply.github.com>
2024-01-09 12:05:30 +0530

    Update SearchAction.java

--- src/test/java/com/qascript/SearchAction.java
+++ src/test/java/com/qascript/SearchAction.java
@@ -1,7 +1,7 @@
 package com.qascript;
 
-import org.junit.Assert;
-import org.junit.Test;
+//import org.junit.Assert;
+//import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
